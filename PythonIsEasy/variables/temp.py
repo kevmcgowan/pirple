02d62de8c2a8d36cf62c847c290c4d1cec39451b
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2024-06-26 11:27:53 +0300

    Python Course Variables Lesson

---
+++ PythonIsEasy/variables/temp.py
@@ -0,0 +1,57 @@
+#one = 1
+#two = 2
+#three = 3
+
+one, two, three = 1, 2, 3
+
+print (one)
+print (two)
+print (three)
+two = 4
+print (two)
+print (one)
+
+pi = 3.14
+
+Decimal = 1.1
+print (Decimal)
+
+StringVar = 'Hello' + '1'
+print (StringVar) 
+
+def FunctionName() :
+    newVar = 'World'
+    print (one)
+    print (newVar)
+    return
+
+FunctionName()
+#print (newVar)
+
+#left = what you're giving the value to
+#right = what the value is
+five = 3+2
+print(five)
+
+count = 0 
+print(count)
+
+count = count +1
+print(count)
+
+count += 1
+print(count)
+
+count = count * 3
+print(count)
+
+count *= 3
+print(count)
+
+count = count / 2
+print(count)
+
+count /= 2
+print(count)
+
+
