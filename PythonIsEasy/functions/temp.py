0ac69660a01d8905ac3bc44026ea5d9d94955b0f
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2024-06-27 16:28:57 +0300

    this is the fuction lesson

---
+++ PythonIsEasy/functions/temp.py
@@ -0,0 +1,59 @@
+"""def FunctionName(Input):
+    Action
+    return Output"""
+
+# def addOne(Number):
+#     Output = Number + 1
+#     return Output
+
+# var = 0
+# print(var)
+# var2 = addOne(var)
+# var3 = addOne(var2)
+# var4 = addOne(2.1+3.4)
+
+# print(var2)
+# print(var3)
+# print(var4)
+
+# def addOneAddTwo(numberOne,numberTwo):
+#     output = numberOne + 1
+#     # output = output + numberTwo + 2
+#     output += numberTwo + 2
+#     return output
+
+# sum = addOneAddTwo(1,2)
+# sum = addOneAddTwo(var2,var3)
+
+# print(sum)
+
+
+# def addOne(number):
+#     number += 1
+#     return number
+# one = addOne(0)
+# two = addOne (one)
+# print(two)
+
+# def hello():
+#     print("i am a beginner")
+# hello()
+# hello()
+
+# def sumA(count):
+#     a = count+1
+#     return a
+# print(sumA)
+
+# def printmesssage(message, ntimes = 1):
+#     print(message * ntimes)
+# printmesssage("hello")
+# printmesssage('hello', 5)
+
+
+num = 1
+def func():
+    num = 4
+    print(num)
+func()
+print(num)
