ff030aaf94a9dbcf9d0c25994a577dbf33140271
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2024-08-24 14:32:31 +0300

    temp file updated in breaking & continuing loops

---
+++ PythonIsEasy/loops/breaking&continuing/temp.py
@@ -0,0 +1,20 @@
+participants = ['Jen','Alex','Tina','Joe','Ben']
+
+# position = 1
+# for name in participants:
+#     if name == 'Alex':
+#         print('About to break')
+#         break
+#     print('About to increment')
+#     position = position + 1
+
+# print(name,"\'s", "Position is:", position)
+# print('Alex')
+
+
+
+for currentIndex in range(len(participants)):
+    if participants [currentIndex ]== 'Joe':
+        break
+
+print(currentIndex+1)
