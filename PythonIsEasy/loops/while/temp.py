328ac50cc55e6bdc9573b16f430030c1dce418a1
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2025-02-08 10:38:10 +0300

    Breaking & Continuing loops

--- PythonIsEasy/loops/while/temp.py
+++ PythonIsEasy/loops/while/temp.py
@@ -22,14 +22,51 @@
 #     print(letters[index])
 #     index = index + 1
 
+participants = ['Jen', 'Alex', 'Tina', 'Joe', 'Ben', 'Tom']
 
-height = 5000
-velocity = 0.49
+position = 1
 
-time = 0
-while (height>0):
-    height = height - velocity
-    time = time + 0.01
+# for name in participants:
+#     if name == 'Tina':
+#         print('About to break')
+#         break
+#     print('About to increment')
+#     position = position + 1
 
-print(height)
-print(time)
+# print(position)
+
+# index = 0
+
+# for currentIndex in range(len(participants)):
+#     print(currentIndex)
+#     if participants[currentIndex] == 'Joe':
+#         print('have breaked')
+#         break
+#     print('Not breaked')
+#     # currentIndex = currentIndex + 1
+
+# print(currentIndex+1)
+
+for number in range(10):
+    if number%3 == 0:
+        print(number)
+        print('is divisible by 3')
+        continue
+    print(number)
+    print('not divisible by 3')
+
+
+
+
+
+
+# height = 5000
+# velocity = 0.49
+
+# time = 0
+# while (height>0):
+#     height = height - velocity
+#     time = time + 0.01
+
+# print(height)
+# print(time)
